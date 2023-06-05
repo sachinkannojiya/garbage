ff2d34d084f3ee14a4593c486e9096c024e8132c
sachin <sachinkannojiya990@gmail.com>
2023-06-05 22:59:42 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,33 @@
+// Examine the document object
+//console.dir(document);
+//console.log(document.documentURI);
+//console.log(document.contentType)
+//document.title="sachin kannojiya";
+// console.log(document.doctype);
+// console.log(doucment.head);
+// console.log(document.body);
+// console.log(document.title);
+// console.log(document.all)
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+//console.log(document.getElementById("header-title"))
+// const title=document.getElementById("header-title");
+// // title.textContent="sachin kannojiya";
+// // title.innerText="satyam Yadav";
+// // title.innerHTML='<h2>abhishek patel</h2>'
+// title.style.borderBottom='solid 3px black'
+
+// // const items=document.getElementsByClassName('list-group-item')
+// // console.log(items)
+
+// const addItem=document.getElementsByClassName("title")
+// // console.log(addItem[1].textContent)
+// addItem[0].style.fontWeight='bold';
+// addItem[0].style.color='green';
+const listItem=document.getElementsByClassName("list-group-item")
+listItem[2].style.backgroundColor = 'green';
+ listItem[0].style.fontWeight='bold';
+ listItem[1].style.fontWeight='bold';
+ listItem[3].style.fontWeight='bold';
+ listItem[2].style.fontWeight='bold';
